5e4e4d35aaf10880e9f90c631177760489b2bdfa
olaborde <oml007@live.com>
2018-11-19 06:26:51 -0500

    printjobs method implemented

--- src/org/launchcode/techjobs/console/TechJobs.java
+++ src/org/launchcode/techjobs/console/TechJobs.java
@@ -111,6 +111,19 @@ public class TechJobs {
     // Print a list of jobs
     private static void printJobs(ArrayList<HashMap<String, String>> someJobs) {
 
-        System.out.println("printJobs is not implemented yet");
+        int jobLen = someJobs.size();
+
+        if (jobLen > 0) {
+            for(HashMap<String, String> job : someJobs) {
+                System.out.println("*****");
+                for(String key : job.keySet()) {
+                    System.out.println(key + ": " + job.get(key));
+                }
+                System.out.println("*****");
+            }
+        }
+        else {
+            System.out.println("jobs not found!");
+        }
     }
 }
